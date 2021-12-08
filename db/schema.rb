69817abb5cd59b7d9748409676fb4deb78b9f9dc
Rostyslav Yemelyanov <emrb.2708@gmail.com>
2021-12-08 16:50:43 +0200

    Add Comment MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_101931) do
+ActiveRecord::Schema.define(version: 2021_12_08_144149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
